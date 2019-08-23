1635ad3fa2d8d9471f0aee736379df9e9000c53f
Gino Miceli <gmiceli@google.com>
2019-08-23 12:09:25 -0400

    Test shadow mask

--- toolkits/landcover/test/int/landsat8.int.test.js
+++ toolkits/landcover/test/int/landsat8.int.test.js
@@ -19,17 +19,34 @@ var Landsat8 = require('../../api.js').Landsat8;
 var TestImage = require('../helpers/test-image.js');
 
 withEarthEngine('Landsat8', function() {
-  it('fmaskCloudsAndShadows() masks cloud pixels', function(done) {
+  it('fmaskCloudsAndShadows() masks cloudy pixels', function(done) {
     var clearPixel = TestImage.create('2018-03-01', {pixel_qa: 0, B4: 12});
     var cloudyPixel = TestImage.create(
         '2018-03-01', {pixel_qa: Landsat8.CLOUD_BIT_MASK, B4: 100});
     var testCollection = ee.ImageCollection([clearPixel, cloudyPixel]);
-    var image = Landsat8(testCollection)
-                    .fmaskCloudsAndShadows()
-                    .getImageCollection()
-                    .mosaic();
+    var l8 = Landsat8(testCollection);
+
+    var maskedL8 = l8.fmaskCloudsAndShadows();
 
     // Verify cloud-free pixel was returned.
+    var image = maskedL8.getImageCollection().mosaic();
+    TestImage.reduce(image).get('B4').evaluate(function(actual) {
+      expect(actual).toEqual(12);
+      done();
+    });
+  });
+
+  it('fmaskCloudsAndShadows() masks shadowy pixels', function(done) {
+    var clearPixel = TestImage.create('2018-03-01', {pixel_qa: 0, B4: 12});
+    var cloudyPixel = TestImage.create(
+        '2018-03-01', {pixel_qa: Landsat8.CLOUD_BIT_SHADOW_MASK, B4: 4});
+    var testCollection = ee.ImageCollection([clearPixel, cloudyPixel]);
+    var l8 = Landsat8(testCollection);
+
+    var maskedL8 = l8.fmaskCloudsAndShadows();
+
+    // Verify cloud shadow-free pixel was returned.
+    var image = maskedL8.getImageCollection().mosaic();
     TestImage.reduce(image).get('B4').evaluate(function(actual) {
       expect(actual).toEqual(12);
       done();

bb2e6947172053e86eb53e9a976069cbe8f9438f
Justin Braaten <jdbcode@users.noreply.github.com>
2019-08-27 11:51:22 -0700

    Address all initial review comments.

--- tutorials/landsat-etm-to-oli-harmonization/script.js
+++ tutorials/landsat-etm-to-oli-harmonization/script.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Copyright 2019 Google LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,25 +19,32 @@
 // ### FUNCTIONS ###
 // ################################################################
 
-// Define coefficients supplied by Roy et. al. 2016 for translating ETM+
+// Define coefficients supplied by Roy et al. (2016) for translating ETM+
 // surface reflectance to OLI surface reflectance.
 var coefficients = {
   itcps: ee.Image.constant([0.0003, 0.0088, 0.0061, 0.0412, 0.0254, 0.0172]).multiply(10000),
   slopes: ee.Image.constant([0.8474, 0.8483, 0.9047, 0.8462, 0.8937, 0.9071])
 };
 
-// Define function to get consistent band names (OLI).
-function oliBands(img) {
-  return ee.Image(ee.Algorithms.If(
-    ee.Algorithms.IsEqual(img.get('SATELLITE'), ee.String('LANDSAT_8')),
-    img.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa']),
-    img.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa'], ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa'])
-  ));
+// Define function to get and rename bands of interest from OLI.
+function renameOLI(img) {
+  return img.select(
+		['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa'],
+		['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2', 'pixel_qa']
+	);
+}
+
+// Define function to get and rename bands of interest from ETM+.
+function renameETM(img) {
+  return img.select(
+		['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa'],
+		['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2', 'pixel_qa']
+  );
 }
 
 // Define function to apply harmonization transformation.
 function etm2oli(img) {
-  return img.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7'])
+  return img.select(['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2'])
     .multiply(coefficients.slopes)
     .add(coefficients.itcps)
     .round()
@@ -58,13 +65,22 @@ function fmask(img) {
 
 // Define function to calculate NBR.
 function calcNBR(img) {
-  return img.normalizedDifference(['B5', 'B7']).rename('NBR');
+  return img.normalizedDifference(['NIR', 'SWIR2']).rename('NBR');
 }
 
-// Define wrapper function to prepare images using above functions.
-function prepImg(img) {
+// Define function to prepare OLI images.
+function prepOLI(img) {
   var orig = img;
-  img = oliBands(img);
+  img = renameOLI(img);
+  img = fmask(img);
+  img = calcNBR(img);
+  return ee.Image(img.copyProperties(orig, orig.propertyNames()));
+}
+
+// Define function to prepare ETM+ images.
+function prepETM(img) {
+  var orig = img;
+  img = renameETM(img);
   img = fmask(img);
   img = etm2oli(img);
   img = calcNBR(img);
@@ -77,11 +93,11 @@ function prepImg(img) {
 
 // Define a point on the north slope of Mount Hood, OR
 // to extract time series for.
-var point = ee.Geometry.Point([-121.70938, 45.43185]);
+var aoi = ee.Geometry.Point([-121.70938, 45.43185]);
 
-// Display point on the map.
-Map.centerObject(point, 9);
-Map.addLayer(point, {color: 'f8766d'}, 'AOI');
+// Display AOI on the map.
+Map.centerObject(aoi, 10);
+Map.addLayer(aoi, {color: 'f8766d'}, 'AOI');
 Map.setOptions('HYBRID');
 
 // Get Landsat surface reflectance collections for OLI, ETM+ and TM sensors.
@@ -91,7 +107,7 @@ var tmCol= ee.ImageCollection('LANDSAT/LT05/C01/T1_SR');
 
 // Define a collection filter.
 var colFilter = ee.Filter.and(
-  ee.Filter.bounds(point),
+  ee.Filter.bounds(aoi),
   ee.Filter.calendarRange(182, 244, 'day_of_year'),
   ee.Filter.lt('CLOUD_COVER', 50),
   ee.Filter.lt('GEOMETRIC_RMSE_MODEL', 10),
@@ -101,25 +117,28 @@ var colFilter = ee.Filter.and(
   )
 );
 
-// Merge collections, apply filter, and prepare images.
+// Filter collections and prepare them for merging.
+oliCol = oliCol.filter(colFilter).map(prepOLI);
+etmCol= etmCol.filter(colFilter).map(prepETM);
+tmCol= tmCol.filter(colFilter).map(prepETM);
+
+// Merge the collections.
 var col = oliCol
   .merge(etmCol)
-  .merge(tmCol)
-  .filter(colFilter)
-  .map(prepImg);
+  .merge(tmCol);
 
-// Extract NBR values for all pixels in the collection that
-// intersect the point. Returns FeatureCollection - copy image
-// properties to each feature.
+// Calculate median NBR for pixels intersecting the AOI for
+// each image in the collection. Add the value as an image property.
 var allObs = col.map(function(img) {
-  return ee.Feature(null, img.reduceRegion({
-    geometry: point,
-    reducer: ee.Reducer.first(),
+  var obs = img.reduceRegion({
+    geometry: aoi,
+    reducer: ee.Reducer.median(),
     scale: 30
-  })).copyProperties(img, img.propertyNames());
+  });
+  return img.set('NBR', obs.get('NBR'));
 });
 
-// Make a chart of all observations in FeatureCollection.
+// Make a chart of all observations where color distinguishes sensor.
 var chartAllObs = ui.Chart.feature.groups(
   allObs, 'system:time_start', 'NBR', 'SATELLITE'
 )
@@ -139,14 +158,15 @@ print(chartAllObs);
 // Need to identify same-year images by a join.
 // Start by adding a 'year' property to each image.
 var col = col.map(function(img) {
-  return img.set('year', img.date().get('year')); 
+  return img.set('year', img.date().get('year'));
 });
 
 // Subset collection to a set of distinct year representatives.
 var distinctYearCol = col.distinct('year');
 
-// Define a filter that identifies images from the complete 
-// collection that match the 'year' from the distinct year collection. 
+// Define a filter that identifies images from the complete
+// collection that match the 'year' from the distinct year collection
+// (distinctYearCol).
 var filter = ee.Filter.equals({leftField: 'year', rightField: 'year'});
 
 // Define a join.
@@ -167,8 +187,8 @@ var medianComp = joinCol.map(function(img) {
 // Make a chart that displays the annual median NBR composite.
 var chartMedianComp = ui.Chart.image.series({
   imageCollection: medianComp,
-  region: point,
-  reducer: ee.Reducer.first(),
+  region: aoi,
+  reducer: ee.Reducer.median(),
   scale: 30,
   xProperty: 'system:time_start',
 })
@@ -187,11 +207,13 @@ print(chartMedianComp);
 // ################################################################
 
 // Roy et al. (2016) Table 2 provides OLS and RMA regression coefficients
-// to translate ETM+ surface reflectance to OLI surface reflectance and vice
+// to transform ETM+ surface reflectance to OLI surface reflectance and vice
 // versa. The above tutorial demonstrates only ETM+ to OLI transformation by OLS
-// regression. Below are functions for all translation options. Replace above
-// lines: 67 (band name standardization) and 69 (transformation) of the `prepImg`
-// function to apply different transformation options.
+// regression. Below are functions for all transformation options. Modify the above
+// `prepOLI` and `prepETM` wrapper functions as needed to add/remove/replace
+// transformation functions. Additionally, use the below `coefficients` dictionary
+// instead of the one defined above (the following one includes all sets of
+// coefficients).
 
 // Define OLS and RMA surface regression coefficients.
 var coefficients = {
@@ -211,7 +233,7 @@ var coefficients = {
 
 // Define function to apply OLS ETM+ to OLI transformation.
 function etm2oli_ols(img) {
-  return img.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7'])
+  return img.select(['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2'])
     .multiply(coefficients.etm2oli_ols.slopes)
     .add(coefficients.etm2oli_ols.itcps)
     .round()
@@ -223,7 +245,7 @@ function etm2oli_ols(img) {
 // Define function to apply OLS OLI to ETM+ transformation.
 function oli2etm_ols(img) {
   return ee.Image(
-    img.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7'])
+    img.select(['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2'])
       .multiply(coefficients.oli2etm_ols.slopes)
       .add(coefficients.oli2etm_ols.itcps)
       .round()
@@ -236,7 +258,7 @@ function oli2etm_ols(img) {
 // Define function to apply RMA OLI to ETM+ transformation.
 function oli2etm_rma(img) {
   return ee.Image(
-    img.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7'])
+    img.select(['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2'])
       .subtract(coefficients.rma.itcps)
       .divide(coefficients.rma.slopes)
       .round()
@@ -249,7 +271,7 @@ function oli2etm_rma(img) {
 // Define function to apply RMA ETM+ to OLI transformation.
 function etm2oli_rma(img) {
   return ee.Image(
-    img.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7'])
+    img.select(['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2'])
       .multiply(coefficients.rma.slopes)
       .add(coefficients.rma.itcps)
       .round()
@@ -257,22 +279,4 @@ function etm2oli_rma(img) {
       .addBands(img.select('pixel_qa'))
       .copyProperties(img, ['system:time_start'])
   );
-}
-
-// Define function to get consistent band names (OLI).
-function oliBands(img) {
-  return ee.Image(ee.Algorithms.If(
-    ee.Algorithms.IsEqual(img.get('SATELLITE'), ee.String('LANDSAT_8')),
-    img.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa']),
-    img.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa'], ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa'])
-  ));
-}
-
-// Define function to get consistent band names (ETM+).
-function etmBands(img) {
-  return ee.Image(ee.Algorithms.If(
-    ee.Algorithms.IsEqual(img.get('SATELLITE'), ee.String('LANDSAT_8')),
-    img.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa'], ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa']),
-    img.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa'])
-  ));
 }

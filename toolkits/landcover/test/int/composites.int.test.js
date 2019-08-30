8d47ced9d40a89dea84fde7181e40930fb3f43cc
Gino Miceli <gmiceli@google.com>
2019-08-30 13:21:41 -0400

    createMedioidFunction int test

--- toolkits/landcover/test/int/composites.int.test.js
+++ toolkits/landcover/test/int/composites.int.test.js
@@ -15,11 +15,11 @@
  * limitations under the License.
  */
 
-var Composites = require('../../impl/composites.js').Composites;
+var lct = require('../../api.js');
 var TestImage = require('../helpers/test-image.js');
 
 withEarthEngine('Composites', function() {
-  fit('createTemporalComposites()', function(done) {
+  it('createTemporalComposites()', function(done) {
     var testCollection = ee.ImageCollection([
       TestImage.create({value: 100}, '2015-12-01'),
       TestImage.create({value: 150}, '2015-12-15'),
@@ -33,7 +33,7 @@ withEarthEngine('Composites', function() {
       TestImage.create({value: 550}, '2016-04-15')
     ]);
 
-    var result = Composites.createTemporalComposites(
+    var result = lct.Composites.createTemporalComposites(
         testCollection,
         /* startDate= */ '2016-01-01',
         /* count= */ 3,
@@ -53,38 +53,59 @@ withEarthEngine('Composites', function() {
   });
 
   it('createMedioidComposite()', function(done) {
-    lct.Landsat8('SR')
-        .filterBounds(geometry)
-        .filterDate('2016-01-01', '2016-01-31')
-        .addDayOfYearBand()
-        .createMedioidComposite('doy')
-        .getImageCollection()
-        .first()
-        .reduceRegion(ee.Reducer.first(), geometry, 1)
-        .evaluate(function(actual, error) {
-          expect(error).toBeUndefined();
-          expect(actual['doy']).toBe(17);
-          done();
-        });
+    // Median idx = 3, so we expect the pixel(s) with idx closest to that
+    // value to be returned.
+    var testCollection = ee.ImageCollection([
+      TestImage.create({idx: 1, value: 101}),
+      TestImage.create({idx: 2, value: 102}),
+      TestImage.create({idx: 3, value: 103}),
+      TestImage.create({idx: 4, value: 104}),
+      TestImage.create({idx: 5, value: 105})
+    ]);
+
+    var composite =
+        lct.Composites.createMedioidComposite(testCollection, 'idx');
+
+    TestImage.reduceConstant(composite).evaluate(function(actual, error) {
+      expect(error).toBeUndefined();
+      expect(actual).toEqual({idx: 3, value: 103});
+      done();
+    });
   });
 
-  it('MedioidFunction in temporalComposites', function(done) {
+  it('createMedioidFunction()', function(done) {
+    // The first and last dates will be excluded by the temporal composite
+    // criteria below. The 5 images in between will be used, with mosaic
+    // built using median index value of 3.
+    var testCollection = ee.ImageCollection([
+      TestImage.create({idx: 0, value: 100}, '2015-12-01'),
+      TestImage.create({idx: 1, value: 101}, '2016-01-01'),
+      TestImage.create({idx: 2, value: 102}, '2016-01-02'),
+      TestImage.create({idx: 3, value: 103}, '2016-01-03'),
+      TestImage.create({idx: 4, value: 104}, '2016-01-04'),
+      TestImage.create({idx: 5, value: 105}, '2016-01-05'),
+      TestImage.create({idx: 6, value: 106}, '2016-06-01')
+    ]);
+
     // With 1 composite, this should be equivalent to the
     // medioidComposite test.
-    var compositor = lct.Composites.createMedioidFunction('date');
-    lct.Landsat8('SR')
-        .filterBounds(geometry)
-        .addDayOfYearBand()
-        .addFractionalYearBand()
-        .createTemporalComposites('2016-01-01', 1, 31, 'day', compositor)
-        .getImageCollection()
-        .first()
-        .reduceRegion(ee.Reducer.first(), geometry, 1)
+    var compositor = lct.Composites.createMedioidFunction('idx');
+    var result = lct.Composites.createTemporalComposites(
+        testCollection,
+        /* startDate= */ '2016-01-01',
+        /* count= */ 1,
+        /* interval= */ 31,
+        /* intervalUnits= */ 'day', compositor);
+
+    result.toList(10)
+        .map(function(img) {
+          return TestImage.reduceConstant(img);
+        })
         .evaluate(function(actual, error) {
           expect(error).toBeUndefined();
-          expect(actual['date']).toBe(1453140198110);
-          expect(actual['doy']).toBe(17);
-          expect(actual['fYear']).toBeCloseTo(2016 + 17 / 365.0, 0.001);
+          // Mean value in each valid 3 month range..
+          expect(actual).toEqual(
+              [{date: 1451779200000, idx: 3, observations: 5, value: 103}]);
           done();
         });
   });

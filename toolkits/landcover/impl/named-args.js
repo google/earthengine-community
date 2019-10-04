a258366a1d14922c5d1cf6e021302e368e52047c
Gino Miceli <228050+gino-m@users.noreply.github.com>
2019-10-04 11:11:16 -0400

    Fix function decl regexp

--- toolkits/landcover/impl/named-args.js
+++ toolkits/landcover/impl/named-args.js
@@ -17,10 +17,22 @@
 
 /**
  * Regular expression used to extract argument names from string represention
- * of a function.
+ * of a function. This does the following:
+ * 
+ *  1. Match only when declaration starts with "function".
+ *  2. Ignores optional space and function name up to first "(".
+ *  3. Captures the contents all the way up to the next ")".
+ * 
+ * This expression assume newlines have already been stripped.
  */
-var FUNCTION_REGEX = /^function\s*(?:.*?)\(\s*(.*)\s*\)/;
+var FUNCTION_REGEX = /^function\s*.*?\((.*?)\)/;
 
+/**
+ * Regular expression for matching newlines. This includes both standard LFs
+ * and Windows CR+LF patterns. We use this to remove newlines, working around
+ * the fact that single-line mode isn't supported by JavaScript RegExp.
+ */
+var NEWLINE_REGEX = /\r?\n/g;
 /**
  * Returns a dictionary of arguments keyed by function argument name. Toolkit
  * functions to simulate the "named args" feature of other popular languages
@@ -33,6 +45,7 @@ var FUNCTION_REGEX = /^function\s*(?:.*?)\(\s*(.*)\s*\)/;
  *   specified function declaration.
  */
 function extractFromFunction(fn, originalArgs) {
+  var decl = fn.toString().replace(NEWLINE_REGEX, ' ');
   // Functions with a single dictionary argument are assumed to have been
   // invoked using named args. This precludes us using named args for functions
   // that take a single dictionary as arguments. 
@@ -40,7 +53,7 @@ function extractFromFunction(fn, originalArgs) {
     return originalArgs[0];
   }
   // Extract arg names using heuristic regex.
-  var regexMatch = fn.toString().match(FUNCTION_REGEX);
+  var regexMatch = decl.match(FUNCTION_REGEX);
   if (!regexMatch || regexMatch.length < 2) {
     throw new Error('Unsupported function declaration:\n' + fn.toString());
   }

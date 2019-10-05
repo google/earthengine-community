4a16591a6f01eff11daa7a95dd053bf7668d6d92
TC Chakraborty <30564889+TC25@users.noreply.github.com>
2019-10-05 10:33:40 -0400

    Update Sample.js

--- tutorials/intro-to-geospatial-analysis/Sample.js
+++ tutorials/intro-to-geospatial-analysis/Sample.js
@@ -1,8 +1,6 @@
 //  Geometries
-
 Map.setCenter(-88, 41.8, 9)
-
-function multi (feature){
+function multi(feature){
   //  Reduce number of vertices in geometry; the number is to specify maximum error in meters
   var simple = feature.simplify(1000);
   //  Find centroid of geometry
@@ -11,7 +9,6 @@ function multi (feature){
   var buff = center.buffer(500);
   //  return variable from function
   return buff}
-
 //  Load Feature Collection of the dissolved boundary of Chicago stored in assets
 var Chic=ee.FeatureCollection('users/tirthankar25/chicago_diss');
 //  Load Feature Collection of the neighborhood-scale Chicago data stored in assets
@@ -20,7 +17,6 @@ var Chic=ee.FeatureCollection('users/tirthankar25/chicago_diss');
 //  var Res=Chic_coll.map(multi);
 //  Add the layer to the map
 // Map.addLayer(Res)
-
 Map.addLayer(Chic, {},'Chicago dissolved')
 //  Find the rectangle that emcompasses the southernmost, westernmost, easternmost, and northernmost
 //  points of the feature
@@ -29,15 +25,12 @@ Map.addLayer(Chic, {},'Chicago dissolved')
 //  var convex = Chic.geometry().convexHull();
 //  Map.addLayer(bound, {},'Chicago bounds');
 //  Map.addLayer(convex, {},'Chicago convex hull');
-
 //  Find the area common to two or more features
 //  var Intersect=bound.intersection(convex,100);
 //  Map.addLayer(Intersect, {},'Bound and convex intersection');
-
 //  Find the area encompassing two or more features; number is the maximum error in meters
 //  var Union=bound.union(convex,100);
 //  Map.addLayer(Union, {},'Bound and convex union');
-
 //  Find area of feature
 //  var Ar=Chic.geometry().area();
 //  Find length of line geometry
@@ -45,10 +38,7 @@ Map.addLayer(Chic, {},'Chicago dissolved')
 //  Find permeter of feature
 //  var peri=Chic.geometry().perimeter();
 //  print (peri)
-
-
 //  Features
-
 //  Create Geometry
 var var_Geometry=ee.Geometry.Polygon(0,0, 40,30, 20, 20, 0, 0);
 //  Create Feature from Geometry
@@ -59,21 +49,17 @@ var var_Feature = ee.Feature(var_Geometry, {Name: ["Feature name","Supreme"], Si
 //  Select a subset of properties and rename them
 var var_Featurenew = var_Feature.select(["Name"], ["Descriptor"]);
 print(var_Featurenew)
-
 //  Images
-
 Map.setCenter(-88, 41.8, 9)
 var Raw = ee.ImageCollection('MODIS/006/MYD11A2');
 var ROI = ee.FeatureCollection('users/tirthankar25/chicago');
 print(Raw)
 var bandsel=Raw.select(0)
 //  var bandsel=Raw.select('LST_Day_1km')
-
 //  var filtered=Raw.filterDate('2002-12-30','2004-4-27');
 //  print(filtered)
 //  var limited=Raw.limit(50);
 //  print(limited)
-
 //  print(bandsel)
 var mean=bandsel.mean();
 Map.addLayer(mean);
@@ -84,15 +70,9 @@ Map.addLayer(mean);
 //  Map.addLayer(mask,{},"mask")
 //  var Masked=clipped.mask(mask)
 //  Map.addLayer(Masked,{min: 20, max: 30, palette: ['blue', 'green', 'red']},'LST_masked')
-
-
 //  var filtered=Raw.filterDate('2002-12-30','2004-4-27');
 //  print(filtered)
-
 //  Image to table example
-
-
-
 var urban=ee.FeatureCollection('users/tirthankar25/chicago');
 //  Function to find mean of pixels in region of interest
 var regions = function(image){ return image.reduceRegions({collection: urban, reducer: ee.Reducer.mean(), scale: 1000, }); }
@@ -104,14 +84,11 @@ print(image)
 //  .select('cf_cvg')
 //  .select('ET').multiply(.1)
 var Final = regions(image);
-
 //  var Final=Final.select(['mean','nbhd_code', 'nbhd'],['Albedo','nbhd_code', 'nbhd']);
 //  Export image
 Export.table.toDrive({
 collection: Final,  description: 'NDVI_all',  fileFormat: 'CSV'});
-
 //  Timelapse example (based on google API example)
-
 var TheGEOMETRY = ee.Geometry.Rectangle([55.1, 25, 55.4, 25.4]);
 Map.addLayer(TheGEOMETRY)
 var AllIMAGES   = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
@@ -126,9 +103,7 @@ var AllIMAGES   = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
   .select(['B4', 'B3', 'B2'])
   //  Make the data 8 bit
   .map(function(image) {
-    
-    return image.multiply(512).uint8()});
-    
+  return image.multiply(512).uint8()});
 Export.video.toDrive({
   collection: AllIMAGES,
   //  Name of file

b2fac961f7f057f9666fafb62f9e1ed24a833c3f
TC Chakraborty <30564889+TC25@users.noreply.github.com>
2019-10-09 22:28:39 -0400

    Update sample.js

--- tutorials/intro-to-geospatial-analysis/sample.js
+++ tutorials/intro-to-geospatial-analysis/sample.js
@@ -3,7 +3,7 @@ Map.setCenter(-88, 41.8, 9);
 //  Load Feature Collection of the dissolved boundary of Chicago stored in assets
 var chic = ee.FeatureCollection('users/tirthankar25/chicago_diss');
 //  Load Feature Collection of the neighborhood-scale Chicago data stored in assets
-var chicColl=ee.FeatureCollection('users/tirthankar25/chicago');
+var chicColl = ee.FeatureCollection('users/tirthankar25/chicago');
 //  Add the layer to the map
 // Map.addLayer(Res);
 Map.addLayer(chic, {color:'red'}, 'Chicago dissolved');
@@ -15,21 +15,21 @@ Map.addLayer(bound, {color:'yellow'},'Bounds');
 var convex = chic.geometry().convexHull();
 Map.addLayer(convex, {color:'blue'},'Convex Hull');
 //  Find the area common to two or more features
-var intersect=bound.intersection(convex,100);
+var intersect = bound.intersection(convex,100);
 Map.addLayer(intersect, {color:'green'},'Bound and convex intersection');
 //  Find the area encompassing two or more features; number is the maximum error in meters
-var union=bound.union(convex,100);
+var union = bound.union(convex,100);
 Map.addLayer(union, {color:'purple'},'Bound and convex union');
 var diff=bound.difference(convex,100);
 Map.addLayer(diff, {color:'purple'},'Bound and convex difference');
 //  Find area of feature
-var ar=chic.geometry().area();
+var ar = chic.geometry().area();
 print (ar)
 //  Find length of line geometry (You get zero since this is a polygon)
-var length=chic.geometry().length();
+var length = chic.geometry().length();
 print (length)
 //  Find permeter of feature
-var peri=chic.geometry().perimeter();
+var peri = chic.geometry().perimeter();
 print (peri)
 
 
@@ -44,7 +44,7 @@ function performMap(feature) {
  return buff;
 }
 //  Map function over Feature Collection
-var mappedCentroid=chicColl.map(performMap);
+var mappedCentroid = chicColl.map(performMap);
 Map.addLayer(mappedCentroid,{},"Mapped centroids")
 
 //  Features
@@ -71,18 +71,18 @@ var bandSel1 = raw.select(0);
 var bandSel2=raw.select('LST_Day_1km');
 var filtered=raw.filterDate('2002-12-30','2004-4-27');
 print(filtered);
-var limited=raw.limit(50);
+var limited = raw.limit(50);
 print(limited);
 print(bandSel1);
 var mean = bandSel1.mean();
-var clipped=mean.clip(roi);
-var calculate=clipped.multiply(.02).subtract(273.15);
+var clipped = mean.clip(roi);
+var calculate = clipped.multiply(.02).subtract(273.15);
 Map.addLayer(calculate,{min: 20, max: 30, palette: ['blue', 'green', 'red']},'LST');
-var mask=calculate.gt(30.8);
+var mask = calculate.gt(30.8);
 Map.addLayer(mask,{},"mask");
-var masked=clipped.mask(mask);
+var masked = clipped.mask(mask);
 Map.addLayer(masked,{min: 10, max: 30, palette: ['blue', 'green', 'red']},'LST_masked');
-var filtered=raw.filterDate('2002-12-30','2004-4-27');
+var filtered = raw.filterDate('2002-12-30','2004-4-27');
 print(filtered);
 
 //  Image to table example
@@ -113,9 +113,9 @@ Export.table.toDrive({
 });
 
 //  Timelapse example (based on google API example);
-var TheGEOMETRY = ee.Geometry.Rectangle([55.1, 25, 55.4, 25.4]);
-Map.addLayer(TheGEOMETRY);
-var AllIMAGES = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
+var geometry = ee.Geometry.Rectangle([55.1, 25, 55.4, 25.4]);
+Map.addLayer(geometry);
+var allImages = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
  //  Filter row and path such that they cover Dubai.
  .filter(ee.Filter.eq('WRS_PATH', 160))
  .filter(ee.Filter.eq('WRS_ROW', 43))
@@ -130,13 +130,13 @@ var AllIMAGES = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
   return image.multiply(512).uint8();
  });
 Export.video.toDrive({
- collection: AllIMAGES,
+ collection: allImages,
  //  Name of file
- description: 'Dubai_timelapse',
+ description: 'dubaiTimelapse',
  //  Quality of video
  dimensions: 720,
  //  FPS of video
  framesPerSecond: 8,
  //  Region of export
- region: TheGEOMETRY
+ region: geometry
 });

9cbac4a0f704550ee4fd363ceafef6fe31c6e069
TC Chakraborty <30564889+TC25@users.noreply.github.com>
2019-10-09 22:32:10 -0400

    Update sample.js

--- tutorials/intro-to-geospatial-analysis/sample.js
+++ tutorials/intro-to-geospatial-analysis/sample.js
@@ -1,68 +1,67 @@
-//  Geometries
+// Geometries
 Map.setCenter(-88, 41.8, 9);
-//  Load Feature Collection of the dissolved boundary of Chicago stored in assets
+// Load Feature Collection of the dissolved boundary of Chicago stored in assets
 var chic = ee.FeatureCollection('users/tirthankar25/chicago_diss');
-//  Load Feature Collection of the neighborhood-scale Chicago data stored in assets
+// Load Feature Collection of the neighborhood-scale Chicago data stored in assets
 var chicColl = ee.FeatureCollection('users/tirthankar25/chicago');
-//  Add the layer to the map
+// Add the layer to the map
 // Map.addLayer(Res);
 Map.addLayer(chic, {color:'red'}, 'Chicago dissolved');
-//  Find the rectangle that emcompasses the southernmost, westernmost, easternmost, and northernmost
-//  points of the feature
+// Find the rectangle that emcompasses the southernmost, westernmost, easternmost, and northernmost
+// points of the feature
 var bound = chic.geometry().bounds();
 Map.addLayer(bound, {color:'yellow'},'Bounds');
-//  Find the polygon covering the extremities of the feature
+// Find the polygon covering the extremities of the feature
 var convex = chic.geometry().convexHull();
 Map.addLayer(convex, {color:'blue'},'Convex Hull');
-//  Find the area common to two or more features
+// Find the area common to two or more features
 var intersect = bound.intersection(convex,100);
 Map.addLayer(intersect, {color:'green'},'Bound and convex intersection');
-//  Find the area encompassing two or more features; number is the maximum error in meters
+// Find the area encompassing two or more features; number is the maximum error in meters
 var union = bound.union(convex,100);
 Map.addLayer(union, {color:'purple'},'Bound and convex union');
 var diff=bound.difference(convex,100);
 Map.addLayer(diff, {color:'purple'},'Bound and convex difference');
-//  Find area of feature
+// Find area of feature
 var ar = chic.geometry().area();
 print (ar)
-//  Find length of line geometry (You get zero since this is a polygon)
+// Find length of line geometry (You get zero since this is a polygon)
 var length = chic.geometry().length();
 print (length)
-//  Find permeter of feature
+// Find permeter of feature
 var peri = chic.geometry().perimeter();
 print (peri)
 
-
 function performMap(feature) {
- //  Reduce number of vertices in geometry; the number is to specify maximum error in meters
+ // Reduce number of vertices in geometry; the number is to specify maximum error in meters
  var simple = feature.simplify(1000);
- //  Find centroid of geometry
+ // Find centroid of geometry
  var center = simple.centroid();
- //  Create buffer around geometry; the number represents the width of buffer in meters
+ // Create buffer around geometry; the number represents the width of buffer in meters
  var buff = center.buffer(500);
- //  return variable from function
+ // return variable from function
  return buff;
 }
-//  Map function over Feature Collection
+// Map function over Feature Collection
 var mappedCentroid = chicColl.map(performMap);
 Map.addLayer(mappedCentroid,{},"Mapped centroids")
 
-//  Features
-//  Create Geometry
+// Features
+// Create Geometry
 var varGeometry = ee.Geometry.Polygon(0, 0, 40, 30, 20, 20, 0, 0);
-//  Create Feature from Geometry
+// Create Feature from Geometry
 var varFeature = ee.Feature(varGeometry, {
  Name: ['Feature name', 'Supreme'],
  Size: [500, 1000]
 });
-//  Get values of a property
+// Get values of a property
 var arr=varFeature.get('Size');
 print (arr);
-//  Select a subset of properties and rename them
+// Select a subset of properties and rename them
 var varFeaturenew = varFeature.select(['Name'], ['Descriptor']);
 print(varFeaturenew);
 
-//  Images
+// Images
 Map.setCenter(-88, 41.8, 9);
 var raw = ee.ImageCollection('MODIS/006/MYD11A2');
 var roi = ee.FeatureCollection('users/tirthankar25/chicago');
@@ -85,38 +84,33 @@ Map.addLayer(masked,{min: 10, max: 30, palette: ['blue', 'green', 'red']},'LST_m
 var filtered = raw.filterDate('2002-12-30','2004-4-27');
 print(filtered);
 
-//  Image to table example
+// Image to table example
 var urban = ee.FeatureCollection('users/tirthankar25/chicago');
-//  Function to find mean of pixels in region of interest
-var regions = function(image) {
+// Function to find mean of pixels in region of interest
+var getRegions = function(image) {
  return image.reduceRegions({
   collection: urban,
   reducer: ee.Reducer.mean(),
   scale: 1000,
  });
 };
-//  Load image
+// Load image
 var image = ee.ImageCollection('MODIS/MYD13A1').filterDate('2002-07-08', '2017-07-08')
  .mean().select('NDVI');
 print(image);
-//  .select("Albedo_BSA_shortwave").multiply(.001);
-//  .select('avg_rad');
-//  .select('cf_cvg');
-//  .select('ET').multiply(.1);
-var Final = regions(image);
-//  var Final=Final.select(['mean','nbhd_code', 'nbhd'],['Albedo','nbhd_code', 'nbhd']);
-//  Export image
+var Final = getRegions(image);
+// Export image
 Export.table.toDrive({
  collection: Final,
  description: 'NDVI_all',
  fileFormat: 'CSV'
 });
 
-//  Timelapse example (based on google API example);
+// Timelapse example (based on google API example);
 var geometry = ee.Geometry.Rectangle([55.1, 25, 55.4, 25.4]);
 Map.addLayer(geometry);
 var allImages = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
- //  Filter row and path such that they cover Dubai.
+ // Filter row and path such that they cover Dubai.
  .filter(ee.Filter.eq('WRS_PATH', 160))
  .filter(ee.Filter.eq('WRS_ROW', 43))
  // Filter cloudy scenes
@@ -125,18 +119,18 @@ var allImages = ee.ImageCollection('LANDSAT/LT05/C01/T1_TOA')
  .filterDate('1984-01-01', '2012-12-30')
  // Select 3-band imagery for the video.
  .select(['B4', 'B3', 'B2'])
- //  Make the data 8 bit
+ // Make the data 8 bit
  .map(function(image) {
   return image.multiply(512).uint8();
  });
 Export.video.toDrive({
  collection: allImages,
- //  Name of file
+ // Name of file
  description: 'dubaiTimelapse',
- //  Quality of video
+ // Quality of video
  dimensions: 720,
- //  FPS of video
+ // FPS of video
  framesPerSecond: 8,
- //  Region of export
+ // Region of export
  region: geometry
 });

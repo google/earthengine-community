b68298acce05f504238f06a15bd9d489b04e2403
Gino Miceli <228050+gino-m@users.noreply.github.com>
2019-10-14 19:26:36 -0400

    Bail if no changes found in specfied dir

--- .travis/require-changes.sh
+++ .travis/require-changes.sh
@@ -2,6 +2,12 @@
 
 set -ev
 
-echo Range: ${TRAVIS_COMMIT_RANGE}
-echo git diff --name-only ${TRAVIS_BRANCH}...${TRAVIS_COMMIT}
-git diff --name-only ${TRAVIS_BRANCH}...${TRAVIS_COMMIT}
+git diff --name-only ${TRAVIS_COMMIT_RANGE}
+DIR="$1"
+CHANGES=`git diff --name-only ${TRAVIS_COMMIT_RANGE} $DIR`
+
+if [ -z "$CHANGES" ]; then
+  echo "No changes in $DIR"
+  travis_terminate 0
+  exit 1
+fi

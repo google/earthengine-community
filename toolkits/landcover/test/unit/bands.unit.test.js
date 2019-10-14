f0a2bd98602fcf05c43f7b4c7672afb3160fcc00
Gino Miceli <228050+gino-m@users.noreply.github.com>
2019-10-14 10:02:37 -0400

    Fix return type and docs

---
+++ toolkits/landcover/test/unit/bands.unit.test.js
@@ -0,0 +1,55 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var Bands = require('../../impl/bands.js').Bands;
+var formatComputedObject =
+    require('../helpers/format-computed-object.js').formatComputedObject;
+
+var createImage = function(values, startDate) {
+  var image = ee.Dictionary(values).toImage().int64();
+  if (startDate) {
+    image = image.set('system:time_start', ee.Date(startDate).millis());
+  }
+  return image;
+};
+
+withEarthEngineStub('Bands', function() {
+  it('getSpectralIndices()', function() {
+    var testImage = ee.Image([0]);
+    var testIndices = ['ndvi', 'evi'];
+    var result = Bands.getSpectralIndices(testImage, testIndices);
+    expect(result.length).toEqual(2);
+    expect(result[0].name()).toEqual('Image');
+    expect(result[1].name()).toEqual('Image');
+  });
+
+  it('getSpectralIndices() fails on bad index name', function() {
+    var testImage = ee.Image([0]);
+    var testIndices = ['ndvi', 'foo'];
+    expect(function() {
+      Bands.getSpectralIndices(testImage, testIndices);
+    }).toThrow();
+  });
+
+  fit('getSpectralIndices() fails on bad index name', function() {
+    var testImage = ee.Image([0]);
+    var testIndices = ee.Array(['ndvi', 'foo']);
+    // expect(function() {
+      Bands.getSpectralIndices(testImage, testIndices);
+    // }).toThrow();
+  });  
+});

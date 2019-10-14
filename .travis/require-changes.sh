b51b681c1e44c7c092739a119393ecbba213c9f7
Gino Miceli <gmiceli@google.com>
2019-10-14 19:53:55 -0400

    Clean up changes script

--- .travis/require-changes.sh
+++ .travis/require-changes.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Exit on error.
 set -ev
 
-git diff --name-only ${TRAVIS_COMMIT_RANGE}
+# Path to check for changes.
 DIR="$1"
+
+# Changed files in this pull request in the specified dir.
 CHANGES=`git diff --name-only ${TRAVIS_COMMIT_RANGE} $DIR`
 
+# Fail if no changes, causing travis.yml to skip running tests.
 if [ -z "$CHANGES" ]; then
-  echo "No changes in $DIR"
+  echo "No changes in $DIR, skipping tests..."
   exit 1
 fi

1efe8bfc208468b978899a0d79472c91af8f35b4
Gino Miceli <gmiceli@google.com>
2019-10-15 14:15:13 -0400

    Add diagnostic output

--- .travis/require-changes.sh
+++ .travis/require-changes.sh
@@ -19,11 +19,13 @@ set -ev
 # Path to check for changes.
 DIR="$1"
 
+echo "Checking for changes in ${DIR} in commits ${TRAVIS_COMMIT_RANGE}"
+
 # Changed files in this pull request in the specified dir.
-CHANGES=`git diff --name-only ${TRAVIS_COMMIT_RANGE} $DIR`
+CHANGES=`git diff --name-only ${TRAVIS_COMMIT_RANGE} ${DIR}`
 
 # Fail if no changes, causing travis.yml to skip running tests.
 if [ -z "$CHANGES" ]; then
-  echo "No changes in $DIR, skipping tests..."
+  echo "No changes in ${DIR}, skipping tests..."
   exit 1
 fi

a1cdf3f8505f1add1770bb336f8886e957710117
Justin Braaten <jdbcode@users.noreply.github.com>
2019-10-22 10:09:00 -0700

    Editorial changes

--- tutorials/ph-ug-temp/ph-ug-temp.js
+++ tutorials/ph-ug-temp/ph-ug-temp.js
@@ -1,56 +1,117 @@
-// Objectives: We want to pull Land Surface Temperature (LST) day data 
-// for 1 year in Uganda.
+/**
+ * @license
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+// #############################################################################
+
+
+// Objectives:
 //
-// We want to be able to describe the data using a time series map using a chart.
-//
-// We want to export a raster to use in public health research.
+// 1. Import Land Surface Temperature (LST) data for 1 year in Uganda.
+// 2. Describe the data using a time series chart.
+// 3. Export processed raster data for use in GIS.
 
 
-// Import vector file with country boundaries from Earth Engine
-var dataset = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017'); // All countries
-var uganda_border = dataset.filter(ee.Filter.eq("country_na","Uganda")); 
-// apply filter where "country names" equals "Uganda"
-print(uganda_border); 
-// print to ensure that new "uganda_border" object just contains polygon for Uganda
+// #############################################################################
 
-  
-// Add region outline to layer - for selected countries
-Map.addLayer(uganda_border);
 
-var modis = ee.ImageCollection('MODIS/MOD11A2'); // read in image collection
-var start = ee.Date('2015-01-01'); // set start date
-var dateRange = ee.DateRange(start, start.advance(1, 'year')); 
-// set end date 1 year
-var mod11a2 = modis.filterDate(dateRange).filterBounds(uganda_border);
-// apply filter on date range, uganda boundary
+// Import country boundaries feature collection.
+var dataset = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017');
 
-var modLSTday = mod11a2.select('LST_Day_1km'); // pull just 1km day LST
+// Apply filter where country name equals Uganda.
+var ugandaBorder = dataset.filter(ee.Filter.eq('country_na', 'Uganda'));
 
-// convert from Kelvin to Celsius
+// Print new "ugandaBorder" object and explorer features and properties.
+// There should only be one feature representing Uganda.
+print(ugandaBorder);
+
+// Add Uganda outline to the Map as a layer.
+Map.centerObject(ugandaBorder, 6);
+Map.addLayer(ugandaBorder);
+
+
+// #############################################################################
+
+
+// Import LST image collection.
+var modis = ee.ImageCollection('MODIS/MOD11A2');
+
+// Define a date range of interest; here, a start date is defined and the end
+// date is determined by advancing 1 year from the start date.
+var start = ee.Date('2015-01-01');
+var dateRange = ee.DateRange(start, start.advance(1, 'year'));
+
+// Filter the LST collection to include only images intersecting the desired
+// date range.
+var mod11a2 = modis.filterDate(dateRange);
+
+// Select only the 1km day LST data band.
+var modLSTday = mod11a2.select('LST_Day_1km');
+
+
+// #############################################################################
+
+
+// Scale to Kelvin and convert to Celsius, set image acquisition time.
 var modLSTc = modLSTday.map(function(img) {
-  return img.multiply(0.02).subtract(273.15).copyProperties(img,['system:time_start','system:time_end']); 
-}); // this will apply the correction
+  return img
+    .multiply(0.02)
+    .subtract(273.15)
+    .copyProperties(img,['system:time_start']);
+});
 
 
-// Charts Long-term Time Series
-var ts1 = ui.Chart.image.series(modLSTc, uganda_border,
- ee.Reducer.mean(), 1000, 'system:time_start').setOptions({
-   title: 'LST Long-Term Time Series',
-   vAxis: {title: 'LST Celsius'},
- });
- print(ts1);
+// #############################################################################
 
-var clippedLSTc = modLSTc.mean().clip(uganda_border);
-// Add clipped image layer to map
-Map.addLayer(clippedLSTc, {'min': 0, 'max': 40, 'palette':"blue,limegreen,yellow,darkorange,red"});
 
-// Export image for use in other software:
+// Chart time series of LST for Uganda in 2015.
+var ts1 = ui.Chart.image.series({
+  imageCollection: modLSTc,
+  region: ugandaBorder,
+  reducer: ee.Reducer.mean(),
+  scale: 1000,
+  xProperty: 'system:time_start'})
+  .setOptions({
+     title: 'LST 2015 Time Series',
+     vAxis: {title: 'LST Celsius'}});
+print(ts1);
 
+
+// #############################################################################
+
+
+// Calculate 8-day mean temperature for Uganda in 2015.
+var clippedLSTc = modLSTc.mean().clip(ugandaBorder);
+
+// Add clipped image layer to the map.
+Map.addLayer(clippedLSTc, {
+  min: 20, max: 40,
+  palette: ['blue', 'limegreen', 'yellow', 'darkorange', 'red']},
+  'Mean temperature, 2015');
+
+
+// #############################################################################
+
+
+// Export the image to your Google Drive account.
 Export.image.toDrive({
-        image: clippedLSTc,
-        description: 'LST_Celsius_ug',
-        region: uganda_border,
-        scale: 1000,
-        crs: 'EPSG:4326',
-        maxPixels: 1e10
-      });
+  image: clippedLSTc,
+  description: 'LST_Celsius_ug',
+  folder: 'my_folder',
+  region: ugandaBorder,
+  scale: 1000,
+  crs: 'EPSG:4326',
+  maxPixels: 1e10});

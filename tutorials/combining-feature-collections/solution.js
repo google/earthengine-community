a1993708561a09b60c5798ecd9fc446cc79df341
Sabrina Szeto <sabrinaszeto@users.noreply.github.com>
2019-10-23 23:05:16 +0200

    Remove trailing spaces.

--- tutorials/combining-feature-collections/solution.js
+++ tutorials/combining-feature-collections/solution.js
@@ -10,14 +10,14 @@ var utahGeometry = ee.Geometry.Polygon([
 var newFeatures = ee.FeatureCollection.randomPoints(utahGeometry, 25, 12);
 var moreNewFeatures = ee.FeatureCollection.randomPoints(utahGeometry, 25, 1);
 
-// Create a new FeatureCollection using a list of the desired FeatureCollections to combine, 
+// Create a new FeatureCollection using a list of the desired FeatureCollections to combine,
 // before flattening them into a single FeatureCollection.
 var combinedFeatureCollection = ee.FeatureCollection([newFeatures,moreNewFeatures]).flatten();
 
 Map.setCenter(-111.445, 39.251, 6);
 
-Map.addLayer(newFeatures, {}, 'New Features');
+Map.addLayer(newFeatures,{},'New Features');
 Map.addLayer(moreNewFeatures,{color:'red'},'More New Features');
-Map.addLayer(combinedFeatureCollection, {color:'yellow'}, 'Combined FeatureCollection');
+Map.addLayer(combinedFeatureCollection,{color:'yellow'},'Combined FeatureCollection');
 
 print(newFeatures, moreNewFeatures, combinedFeatureCollection);

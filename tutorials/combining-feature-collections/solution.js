46de7c69c4af2595a1bf675a3b97dc9e20ffe1f4
Sabrina Szeto <sabrinaszeto@users.noreply.github.com>
2019-10-23 23:09:34 +0200

    Update solution.js

--- tutorials/combining-feature-collections/solution.js
+++ tutorials/combining-feature-collections/solution.js
@@ -12,12 +12,12 @@ var moreNewFeatures = ee.FeatureCollection.randomPoints(utahGeometry, 25, 1);
 
 // Create a new FeatureCollection using a list of the desired FeatureCollections to combine,
 // before flattening them into a single FeatureCollection.
-var combinedFeatureCollection = ee.FeatureCollection([newFeatures,moreNewFeatures]).flatten();
+var combinedFeatureCollection = ee.FeatureCollection([newFeatures, moreNewFeatures]).flatten();
 
 Map.setCenter(-111.445, 39.251, 6);
 
 Map.addLayer(newFeatures, {}, 'New Features');
-Map.addLayer(moreNewFeatures, {color:'red'}, 'More New Features');
-Map.addLayer(combinedFeatureCollection, {color:'yellow'}, 'Combined FeatureCollection');
+Map.addLayer(moreNewFeatures, {color: 'red'}, 'More New Features');
+Map.addLayer(combinedFeatureCollection, {color: 'yellow'}, 'Combined FeatureCollection');
 
 print(newFeatures, moreNewFeatures, combinedFeatureCollection);

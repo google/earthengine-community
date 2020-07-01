8ec569c7ca9ca981292dda3a76349da0f13d9b9e
Mohamed Ibrahim <msibrahim@google.com>
2020-07-01 03:39:51 -0400

    Restructured server

--- app-creator/main.go
+++ app-creator/main.go
@@ -1,39 +1,65 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
-	"regexp"
-
-	"github.com/labstack/echo"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	// Creating echo object.
-	e := echo.New()
+	/**
+	* Creating shared logger instance.
+	 */
+	l := log.New(os.Stdout, "app-creator", log.LstdFlags)
 
-	// Setting static directory.
-	e.Static("/static", "static")
+	/**
+	* Setting up handlers.
+	 */
+	indexHandler := handlers.NewIndexHandler(l)
 
-	// Serving index.html on "/" route.
-	e.File("/", "static/index.html")
+	/**
+	* Registering handlers with a new server mux.
+	 */
+	serverMux := http.NewServeMux()
+	serverMux.Handle("/", indexHandler)
 
-	// Check if a PORT env variable is set.
-	port := os.Getenv("PORT")
-
-	// Strip any leading leading colons and make sure it only contains numbers.
-	re := regexp.MustCompile(`^[:]+?([0-9]+)$`)
-	port = string(re.ReplaceAll([]byte(port), []byte("$1"))[:])
-
-	reAllNumbers := regexp.MustCompile(`^[0-9]+$`)
-
-	// Default to port 8080 if environment variable is not set or is invalid.
-	if port == "" || !reAllNumbers.MatchString(port) {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+	/**
+	* Setting up server instance.
+	 */
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      serverMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
 
-	// Starting server.
-	log.Printf("Listening on port %s", port)
-	e.Logger.Fatal(e.Start(":" + port))
+	/**
+	* Start server on a new thread. This is a blocking operation
+	* so we need to run it in the background.
+	 */
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	/**
+	* Handle termination gracefully by listening to Interrupt and Kill signals
+	* and waiting 30 seconds to make sure any handlers being executed get to
+	* complete in time.
+	 */
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Shutting down gracefully")
+
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(timeoutContext)
 }

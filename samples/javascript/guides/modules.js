69912abad5b848e5fc5979e9fc3aec45bac7e20d
The Google Earth Engine Community Authors <noreply@google.com>
2021-10-18 09:15:58 -0700

    Internal change
    PiperOrigin-RevId: 403977637

--- samples/javascript/guides/modules.js
+++ samples/javascript/guides/modules.js
@@ -20,21 +20,25 @@
  */
 
 // [START earthengine__modules__exports]
-exports.doc = 'The Foo module is a demonstration of script modules.' +
-    '\n It contains a foo function that returns a greeting string. ' +
-    '\n It also contains a bar object representing the current date.' +
-    '\n' +
-    '\n foo(arg):' +
-    '\n   @param {ee.String} arg The name to which the greeting should be addressed' +
-    '\n   @return {ee.String} The complete greeting.' +
-    '\n' +
-    '\n bar:' +
-    '\n   An ee.Date object containing the time at which the object was created.';
+/**
+ * The Foo module is a demonstration of script modules.
+ * It contains a foo function that returns a greeting string.
+ * It also contains a bar object representing the current date.
+ * @module Modules/FooModule
+ */
 
+/**
+ * Returns a greeting string.
+ * @param {ee.String} arg The name to which the greeting should be addressed
+ * @return {ee.String} The complete greeting.
+ */
 exports.foo = function(arg) {
   return 'Hello, ' + arg + '!  And a good day to you!';
 };
 
+/**
+ * An ee.Date object containing the time at which the object was created.
+ */
 exports.bar = ee.Date(Date.now());
 // [END earthengine__modules__exports]
 

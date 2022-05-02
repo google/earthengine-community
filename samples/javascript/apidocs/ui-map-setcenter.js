b092fd9d40ef19bd9202178139e72a6d3d88a925
Justin Braaten <braaten@google.com>
2022-05-02 16:52:57 -0700

    Internal change
    PiperOrigin-RevId: 446063744

--- samples/javascript/apidocs/ui-map-setcenter.js
+++ samples/javascript/apidocs/ui-map-setcenter.js
@@ -18,9 +18,8 @@
 // Define a ui.Map widget.
 var map = ui.Map();
 
-// Set the position and optional zoom level of the map. Arguments must be
-// unnamed, in order: longitude (x), latitude (y), zoom level. Latitude must be
+// Set the position and optional zoom level of the map. Latitude must be
 // within [-85, 85].
-map.setCenter(-123.6, 47.7, 9);
+map.setCenter({lon: -123.6, lat: 47.7, zoom: 9});
 print(map);
 // [END earthengine__apidocs__ui_map_setcenter]

6623802cb34f2eb36a976ab3dad3985b1995a4be
Justin Braaten <braaten@google.com>
2022-07-20 16:37:51 -0700

    [API code snippets] Add a couple of Export.image.* API code snippets
    PiperOrigin-RevId: 462257777

--- samples/javascript/apidocs/export-image-toasset.js
+++ samples/javascript/apidocs/export-image-toasset.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 The Google Earth Engine Community Authors
+ * Copyright 2022 The Google Earth Engine Community Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-// [START earthengine__apidocs__export_image_todrive]
+// [START earthengine__apidocs__export_image_toasset]
 // A Landsat 8 surface reflectance image.
 var image = ee.Image('LANDSAT/LC08/C02/T1_L2/LC08_044034_20210508')
   .select(['SR_B.']);  // reflectance bands
@@ -23,10 +23,10 @@ var image = ee.Image('LANDSAT/LC08/C02/T1_L2/LC08_044034_20210508')
 var region = ee.Geometry.BBox(-122.24, 37.13, -122.11, 37.20);
 
 // Set the export "scale" and "crs" parameters.
-Export.image.toDrive({
+Export.image.toAsset({
   image: image,
   description: 'image_export',
-  folder: 'ee_demos',
+  assetId: 'projects/<project-name>/assets/<asset-name>',  // <> modify these
   region: region,
   scale: 30,
   crs: 'EPSG:5070'
@@ -36,37 +36,35 @@ Export.image.toDrive({
 // over the output grid. Here, "crsTransform" is set to align the output grid
 // with the grid of another dataset. To view an image's CRS transform:
 // print(image.projection())
-Export.image.toDrive({
+Export.image.toAsset({
   image: image,
   description: 'image_export_crstransform',
-  folder: 'ee_demos',
+  assetId: 'projects/<project-name>/assets/<asset-name>',  // <> modify these
   region: region,
   crsTransform: [30, 0, -2493045, 0, -30, 3310005],
   crs: 'EPSG:5070'
 });
 
 // If the export has more than 1e8 pixels, set "maxPixels" higher.
-Export.image.toDrive({
+Export.image.toAsset({
   image: image,
   description: 'image_export_maxpixels',
-  folder: 'ee_demos',
+  assetId: 'projects/<project-name>/assets/<asset-name>',  // <> modify these
   region: region,
   scale: 30,
   crs: 'EPSG:5070',
   maxPixels: 1e13
 });
 
-// Export a Cloud Optimized GeoTIFF (COG) by setting the "cloudOptimized"
-// parameter to true.
-Export.image.toDrive({
-  image: image,
-  description: 'image_export_cog',
-  folder: 'ee_demos',
+// The default "pyramidingPolicy" is mean. If data are categorical,
+// consider mode.
+Export.image.toAsset({
+  image: image.select('SR_B5'),
+  description: 'image_export_pyramiding',
+  assetId: 'projects/<project-name>/assets/<asset-name>',  // <> modify these
   region: region,
   scale: 30,
   crs: 'EPSG:5070',
-  formatOptions: {
-    cloudOptimized: true
-  }
+  pyramidingPolicy: {SR_B5: 'mode'}
 });
-// [END earthengine__apidocs__export_image_todrive]
+// [END earthengine__apidocs__export_image_toasset]

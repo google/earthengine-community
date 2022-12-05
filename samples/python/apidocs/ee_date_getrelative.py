f2ce87b72bb09f5338727d815f68a0a58466f568
Gino Miceli <gmiceli@google.com>
2022-12-05 10:00:08 -0800

    Internal fix.
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/earthengine-community/pull/590 from jdbcode:broken_sync 08706af8c4643fdd123bb561ecb479e1e7c7d741
    PiperOrigin-RevId: 493039125

---
+++ samples/python/apidocs/ee_date_getrelative.py
@@ -0,0 +1,29 @@
+# Copyright 2021 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_date_getrelative]
+date = ee.Date('2021-4-30T07:15:31.24')
+
+print('0-based month of year:', date.getRelative('year', 'year').getInfo())
+print('0-based week of year:', date.getRelative('week', 'year').getInfo())
+print('0-based day of year:', date.getRelative('day', 'year').getInfo())
+print('0-based day of month:', date.getRelative('day', 'month').getInfo())
+print('0-based minute of day:', date.getRelative('minute', 'day').getInfo())
+print('0-based second of minute:',
+      date.getRelative('second', 'minute').getInfo())
+
+# 0 is returned when unit argument is larger than inUnit argument.
+print('0-based year of month (bad form):',
+      date.getRelative('year', 'month').getInfo())
+# [END earthengine__apidocs__ee_date_getrelative]
---
+++ samples/python/apidocs/ee_daterange_intersects.py
@@ -0,0 +1,29 @@
+# Copyright 2021 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_daterange_intersects]
+# A series of ee.DateRange objects.
+date_range_1 = ee.DateRange('2017-06-24', '2017-07-24')
+date_range_2 = ee.DateRange('2017-06-30', '2018-07-10')
+date_range_3 = ee.DateRange('1970-06-24', '1971-07-24')
+date_range_4 = ee.DateRange('2017-06-25', '2017-07-25')
+
+# Determine if an ee.DateRange intersects another.
+print('Does date_range_1 contain date_range_2?',
+      date_range_1.intersects(date_range_2).getInfo())
+print('Does date_range_1 contain date_range_3?',
+      date_range_1.intersects(date_range_3).getInfo())
+print('Does date_range_1 contain date_range_4?',
+      date_range_1.intersects(date_range_4).getInfo())
+# [END earthengine__apidocs__ee_daterange_intersects]

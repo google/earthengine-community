334563b1d74254dc51d7c3e83a0a78090d12d4e7
The Google Earth Engine Community Authors <earth-engine-community-copybara@google.com>
2023-02-07 16:22:26 -0800

    Make SMAP anomaly Python code work in computed asset creation
    PiperOrigin-RevId: 507915667

--- datasets/smap_anomaly_l3.py
+++ datasets/smap_anomaly_l3.py
@@ -8,88 +8,78 @@ This code calculates a normalized anomaly with SMAP L3 AM and PM data
    centered around day t. t is the current day-15.
 2. Create QA mask
 3. Subtract the climatology from the current average to get the anomaly
-4. Divide the anomaly by the climatology to normalize the data
-5. Apply QA mask to normalized anomaly
+4. Apply QA mask to the anomaly
 """
 
 import datetime
-import pytz
-
 import ee
 
-SMAPL3 = ee.ImageCollection('NASA/SMAP/SPL3SMP_E/005_RAW')
 
-# getting today's date minus three days (3 days ago for processing lag)
-anomalydate = ee.Date(datetime.datetime.now(pytz.UTC)).advance(
-    -3, 'day').format('YYYY-MM-DD')
-anomalymonth = ee.Number.parse(
-    ee.Date(datetime.datetime.now(pytz.UTC)).advance(-3, 'day').format('M'))
-anomalyday = ee.Number.parse(
-    ee.Date(datetime.datetime.now(pytz.UTC)).advance(-3, 'day').format('d'))
+# pytype:disable=attribute-error
 
-# create list of years to calculate climatology
-startyear = 2015  # first year of SMAP data
-currentyear = ee.Number.parse(
-    ee.Date(datetime.datetime.now(pytz.UTC)).advance(-3, 'day').format('YYYY'))
-years = ee.List.sequence(2015, currentyear)
+def anomaly(image):
+  SMAPL3 = ee.ImageCollection('NASA/SMAP/SPL3SMP_E/005_RAW')
+  current_date = ee.Date(datetime.datetime.now())
 
-# select SM for current day
-currentdaySM = SMAPL3.filter(ee.Filter.date(anomalydate))
+  # create list of years to calculate climatology
+  start_year = 2015  # first year of SMAP data
+  current_year = current_date.get('year')
+  image_year = ee.Date(image.get('system:time_start')).get('year')
+  years = ee.List.sequence(start_year, current_year).remove(image_year)
 
-soilmoisture_AM = currentdaySM.select('soil_moisture_am').toBands()
-soilmoisture_PM = currentdaySM.select('soil_moisture_pm').toBands()
+  soilmoisture_AM = image.select('soil_moisture_am')
+  soilmoisture_PM = image.select('soil_moisture_pm')
 
-# select QA values
-soilmoisture_AM_qamask = currentdaySM.select('retrieval_qual_flag_am').toBands()
-soilmoisture_PM_qamask = currentdaySM.select('retrieval_qual_flag_pm').toBands()
+  # select QA values
+  soilmoisture_AM_qamask = image.select('retrieval_qual_flag_am')
+  soilmoisture_PM_qamask = image.select('retrieval_qual_flag_pm')
 
-# invert QA values to create mask
-QA_AM_mask = soilmoisture_AM_qamask.eq(0)
-QA_PM_mask = soilmoisture_PM_qamask.eq(0)
+  # invert QA values to create mask
+  QA_AM_mask = soilmoisture_AM_qamask.eq(0)
+  QA_PM_mask = soilmoisture_PM_qamask.eq(0)
+
+  # create climatologies
+  # function calculating image of climatology means for AM
+  def am_clim(year):
+    date_object = current_date.update(year=year)
+    begindate = ee.Date(date_object).advance(-15, 'day')
+    enddate = ee.Date(date_object).advance(+16, 'day')
+    annual = SMAPL3.filter(ee.Filter.date(
+        begindate, enddate)).select('soil_moisture_am').mean()
+    return annual.rename('soil_moisture_am_anomaly_15d')
+
+  annualSM_AM = ee.ImageCollection.fromImages(years.map(am_clim))
+
+  # calculate average of all years
+  climSM_AM = annualSM_AM.toBands().reduce(ee.Reducer.mean())
+
+  # repeat above function for PM
+  def pm_clim(year):
+    date_object = current_date.update(year=year)
+    begindate = ee.Date(date_object).advance(-15, 'day')
+    enddate = ee.Date(date_object).advance(+15, 'day')
+    annual = SMAPL3.filter(ee.Filter.date(
+        begindate, enddate)).select('soil_moisture_pm').mean()
+    return annual.rename('soil_moisture_pm_anomaly_15d')
+
+  annualSM_PM = ee.ImageCollection.fromImages(years.map(pm_clim))
+
+  # calculate average of all years
+  climSM_PM = annualSM_PM.toBands().reduce(ee.Reducer.mean())
+
+  # calculate anomaly
+  SM_anomaly_AM = soilmoisture_AM.subtract(climSM_AM).rename(
+      'soil_moisture_am_anomaly')
+  SM_anomaly_PM = soilmoisture_PM.subtract(climSM_PM).rename(
+      'soil_moisture_pm_anomaly')
+
+  # apply QA mask
+  SM_anomaly_AM_masked = SM_anomaly_AM.updateMask(QA_AM_mask)
+  SM_anomaly_PM_masked = SM_anomaly_PM.updateMask(QA_PM_mask)
+
+  return SM_anomaly_AM_masked, SM_anomaly_PM_masked
 
 
-# create climatologies
-# function calculating image of climatology means for AM
-def am_clim(year):
-  date_object = ee.Date.fromYMD(year, anomalymonth, anomalyday)
-  begindate = ee.Date(date_object).advance(-15, 'day')
-  enddate = ee.Date(date_object).advance(+15, 'day')
-  annual = (
-      SMAPL3.filter(ee.Filter.date(begindate,
-                                   enddate)).select('soil_moisture_am').mean())
-  return (annual.set('year', year).set('system:time_start',
-                                       ee.Date.fromYMD(year, 1, 1)))
-
-
-annualSM_AM = ee.ImageCollection.fromImages([am_clim(year) for year in years])
-
-# calculate average of all years
-climSM_AM = annualSM_AM.toBands().reduce(ee.Reducer.mean())
-
-
-# repeat above function for PM
-def pm_clim(year):
-  date_object = ee.Date.fromYMD(year, anomalymonth, anomalyday)
-  begindate = ee.Date(date_object).advance(-15, 'day')
-  enddate = ee.Date(date_object).advance(+15, 'day')
-  annual = (
-      SMAPL3.filter(ee.Filter.date(begindate,
-                                   enddate)).select('soil_moisture_pm').mean())
-  return annual
-
-
-annualSM_PM = ee.ImageCollection.fromImages([pm_clim(year) for year in years])
-
-# calculate average of all years
-climSM_PM = annualSM_PM.toBands().reduce(ee.Reducer.mean())
-
-# calculate anomaly
-SM_anomaly_AM = soilmoisture_AM.subtract(climSM_AM)
-SM_anomaly_PM = soilmoisture_PM.subtract(climSM_PM)
-
-SM_anomaly_AM_normalized = SM_anomaly_AM.divide(climSM_AM)
-SM_anomaly_PM_normalized = SM_anomaly_PM.divide(climSM_PM)
-
-# apply QA mask
-SM_anomaly_AM_masked = SM_anomaly_AM_normalized.updateMask(QA_AM_mask)
-SM_anomaly_PM_masked = SM_anomaly_PM_normalized.updateMask(QA_PM_mask)
+def apply(image):
+  am, pm = anomaly(image)
+  return image.addBands([am, pm])
---
+++ datasets/smap_anomaly_l4.py
@@ -0,0 +1,54 @@
+"""SMAP L4 anomaly.
+
+Code by Karyn Tabor (NASA).
+
+This code calculates a normalized anomaly with SMAP L4 data
+1. Calculate the climatology by averaging 31 days of SMAP L4 daily
+   data over every year in the record. The 31-day window is
+   centered around day t. t is the current day-15.
+2. Subtract the climatology from the current average to get the anomaly
+"""
+
+import datetime
+import ee
+
+
+# pytype:disable=attribute-error
+
+def anomaly(image):
+  SMAPL4 = ee.ImageCollection('NASA/SMAP/SPL4SMGP/007_RAW')
+  current_date = ee.Date(datetime.datetime.now())
+
+  # create list of years to calculate climatology
+  start_year = 2015  # first year of SMAP data
+  current_year = current_date.get('year')
+  image_year = ee.Date(image.get('system:time_start')).get('year')
+  years = ee.List.sequence(start_year, current_year).remove(image_year)
+
+  soilmoisture = image.select('sm_surface')
+
+  # create climatologies
+  # function calculating image of climatology means 
+  def clim(year):
+    date_object = current_date.update(year=year)
+    begindate = ee.Date(date_object).advance(-15, 'day')
+    enddate = ee.Date(date_object).advance(+16, 'day')
+    annual = SMAPL4.filter(ee.Filter.date(
+        begindate, enddate)).select('sm_surface').mean()
+    return annual.rename('soil_moisture_anomaly_15d')
+
+  annualSM = ee.ImageCollection.fromImages(years.map(clim))
+
+  # calculate average of all years
+  climSM = annualSM.toBands().reduce(ee.Reducer.mean())
+
+  # calculate anomaly
+  SM_anomaly = soilmoisture.subtract(climSM).rename(
+      'sm_surface_anomaly')
+
+  return SM_anomaly
+
+
+def apply(image):
+  computed_anomaly = anomaly(image)
+  return image.addBands([computed_anomaly])

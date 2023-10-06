3817138bd32bcb3b95ba89184dbead945b0ac388
Justin Braaten <braaten@google.com>
2023-10-06 09:08:20 -0700

    Convert Geometry.LineString API code snippets from JS to Py
    PiperOrigin-RevId: 571353422

---
+++ samples/python/apidocs/ee_geometry_linestring_union.py
@@ -0,0 +1,37 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_linestring_union]
+# Define a LineString object.
+linestring = ee.Geometry.LineString([[-122.09, 37.42], [-122.08, 37.43]])
+
+# Define other inputs.
+input_geom = ee.Geometry.BBox(-122.085, 37.415, -122.075, 37.425)
+
+# Apply the union method to the LineString object.
+linestring_union = linestring.union(right=input_geom, maxError=1)
+
+# Print the result.
+display('linestring.union(...) =', linestring_union)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(linestring, {'color': 'black'}, 'Geometry [black]: linestring')
+m.add_ee_layer(input_geom, {'color': 'blue'}, 'Parameter [blue]: input_geom')
+m.add_ee_layer(
+    linestring_union, {'color': 'red'}, 'Result [red]: linestring.union'
+)
+m
+# [END earthengine__apidocs__ee_geometry_linestring_union]

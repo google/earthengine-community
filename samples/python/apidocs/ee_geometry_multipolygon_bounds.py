b3f92846680dd03ba915857f7be6ca6f674326d0
Justin Braaten <braaten@google.com>
2023-10-06 09:33:08 -0700

    Convert Geometry.MultiPolygon API code snippets from JS to Py
    PiperOrigin-RevId: 571359199

---
+++ samples/python/apidocs/ee_geometry_multipolygon_bounds.py
@@ -0,0 +1,43 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_multipolygon_bounds]
+# Define a MultiPolygon object.
+multipolygon = ee.Geometry.MultiPolygon([
+    [[
+        [-122.092, 37.424],
+        [-122.086, 37.418],
+        [-122.079, 37.425],
+        [-122.085, 37.423],
+    ]],
+    [[[-122.081, 37.417], [-122.086, 37.421], [-122.089, 37.416]]],
+])
+
+# Apply the bounds method to the MultiPolygon object.
+multipolygon_bounds = multipolygon.bounds()
+
+# Print the result.
+display('multipolygon.bounds(...) =', multipolygon_bounds)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(
+    multipolygon, {'color': 'black'}, 'Geometry [black]: multipolygon'
+)
+m.add_ee_layer(
+    multipolygon_bounds, {'color': 'red'}, 'Result [red]: multipolygon.bounds'
+)
+m
+# [END earthengine__apidocs__ee_geometry_multipolygon_bounds]

6443b8a000a0f4d0efc286a7d09ae098ff1c3a5a
Justin Braaten <braaten@google.com>
2023-10-06 09:09:48 -0700

    Convert Geometry.Point API code snippets from JS to Py
    PiperOrigin-RevId: 571353753

---
+++ samples/python/apidocs/ee_geometry_point_bounds.py
@@ -0,0 +1,31 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_point_bounds]
+# Define a Point object.
+point = ee.Geometry.Point(-122.082, 37.42)
+
+# Apply the bounds method to the Point object.
+point_bounds = point.bounds()
+
+# Print the result.
+display('point.bounds(...) =', point_bounds)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(point, {'color': 'black'}, 'Geometry [black]: point')
+m.add_ee_layer(point_bounds, {'color': 'red'}, 'Result [red]: point.bounds')
+m
+# [END earthengine__apidocs__ee_geometry_point_bounds]
---
+++ samples/python/apidocs/ee_geometry_point_distance.py
@@ -0,0 +1,34 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_point_distance]
+# Define a Point object.
+point = ee.Geometry.Point(-122.082, 37.42)
+
+# Define other inputs.
+input_geom = ee.Geometry.Point(-122.090, 37.423)
+
+# Apply the distance method to the Point object.
+point_distance = point.distance(right=input_geom, maxError=1)
+
+# Print the result.
+display('point.distance(...) =', point_distance)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(point, {'color': 'black'}, 'Geometry [black]: point')
+m.add_ee_layer(input_geom, {'color': 'blue'}, 'Parameter [blue]: input_geom')
+m
+# [END earthengine__apidocs__ee_geometry_point_distance]

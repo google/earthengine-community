a3a734c24c8edc84cc076f1a28f81dec634ab52c
Justin Braaten <braaten@google.com>
2023-10-06 09:14:01 -0700

    Convert Geometry.Rectangle API code snippets from JS to Py
    PiperOrigin-RevId: 571354713

---
+++ samples/python/apidocs/ee_geometry_rectangle_perimeter.py
@@ -0,0 +1,30 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_rectangle_perimeter]
+# Define a Rectangle object.
+rectangle = ee.Geometry.Rectangle(-122.09, 37.42, -122.08, 37.43)
+
+# Apply the perimeter method to the Rectangle object.
+rectangle_perimeter = rectangle.perimeter(maxError=1)
+
+# Print the result.
+display('rectangle.perimeter(...) =', rectangle_perimeter)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(rectangle, {'color': 'black'}, 'Geometry [black]: rectangle')
+m
+# [END earthengine__apidocs__ee_geometry_rectangle_perimeter]

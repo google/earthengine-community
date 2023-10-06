63fc2efc3f6b901c2c6727903a98b07b423cb642
Justin Braaten <braaten@google.com>
2023-10-06 08:58:13 -0700

    Convert Geometry.MultiLineString API code snippets from JS to Py
    PiperOrigin-RevId: 571350951

---
+++ samples/python/apidocs/ee_geometry_multilinestring_union.py
@@ -0,0 +1,44 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_multilinestring_union]
+# Define a MultiLineString object.
+multilinestring = ee.Geometry.MultiLineString([
+    [[-122.088, 37.418], [-122.086, 37.422], [-122.082, 37.418]],
+    [[-122.087, 37.416], [-122.083, 37.416], [-122.082, 37.419]],
+])
+
+# Define other inputs.
+input_geom = ee.Geometry.BBox(-122.085, 37.415, -122.075, 37.425)
+
+# Apply the union method to the MultiLineString object.
+multilinestring_union = multilinestring.union(right=input_geom, maxError=1)
+
+# Print the result.
+display('multilinestring.union(...) =', multilinestring_union)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(
+    multilinestring, {'color': 'black'}, 'Geometry [black]: multilinestring'
+)
+m.add_ee_layer(input_geom, {'color': 'blue'}, 'Parameter [blue]: input_geom')
+m.add_ee_layer(
+    multilinestring_union,
+    {'color': 'red'},
+    'Result [red]: multilinestring.union',
+)
+m
+# [END earthengine__apidocs__ee_geometry_multilinestring_union]
---
+++ samples/python/apidocs/ee_geometry_multilinestring_withindistance.py
@@ -0,0 +1,43 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_multilinestring_withindistance]
+# Define a MultiLineString object.
+multilinestring = ee.Geometry.MultiLineString([
+    [[-122.088, 37.418], [-122.086, 37.422], [-122.082, 37.418]],
+    [[-122.087, 37.416], [-122.083, 37.416], [-122.082, 37.419]],
+])
+
+# Define other inputs.
+input_geom = ee.Geometry.Point(-122.090, 37.423)
+
+# Apply the withinDistance method to the MultiLineString object.
+multilinestring_within_distance = multilinestring.withinDistance(
+    right=input_geom, distance=500, maxError=1
+)
+
+# Print the result.
+display(
+    'multilinestring.withinDistance(...) =', multilinestring_within_distance
+)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(
+    multilinestring, {'color': 'black'}, 'Geometry [black]: multilinestring'
+)
+m.add_ee_layer(input_geom, {'color': 'blue'}, 'Parameter [blue]: input_geom')
+m
+# [END earthengine__apidocs__ee_geometry_multilinestring_withindistance]

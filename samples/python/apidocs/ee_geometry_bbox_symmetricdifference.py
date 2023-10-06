8c0b867e233f746d4864c5c4d29a16f1954dadfe
Justin Braaten <braaten@google.com>
2023-10-06 09:01:53 -0700

    Convert Geometry.BBox API code snippets from JS to Py
    PiperOrigin-RevId: 571351916

---
+++ samples/python/apidocs/ee_geometry_bbox_symmetricdifference.py
@@ -0,0 +1,41 @@
+# Copyright 2023 The Google Earth Engine Community Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START earthengine__apidocs__ee_geometry_bbox_symmetricdifference]
+# Define a BBox object.
+bbox = ee.Geometry.BBox(-122.09, 37.42, -122.08, 37.43)
+
+# Define other inputs.
+input_geom = ee.Geometry.BBox(-122.085, 37.415, -122.075, 37.425)
+
+# Apply the symmetricDifference method to the BBox object.
+bbox_symmetric_difference = bbox.symmetricDifference(
+    right=input_geom, maxError=1
+)
+
+# Print the result.
+display('bbox.symmetricDifference(...) =', bbox_symmetric_difference)
+
+# Display relevant geometries on the map.
+m = geemap.Map()
+m.set_center(-122.085, 37.422, 15)
+m.add_ee_layer(bbox, {'color': 'black'}, 'Geometry [black]: bbox')
+m.add_ee_layer(input_geom, {'color': 'blue'}, 'Parameter [blue]: input_geom')
+m.add_ee_layer(
+    bbox_symmetric_difference,
+    {'color': 'red'},
+    'Result [red]: bbox.symmetricDifference',
+)
+m
+# [END earthengine__apidocs__ee_geometry_bbox_symmetricdifference]
